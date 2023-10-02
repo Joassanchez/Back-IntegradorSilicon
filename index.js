27ed8e5e8766c97a0f2ad0f98ce768fb96da8a00
Joassanchez <Joassanchez03@gmail.com>
2023-10-02 09:26:23 -0300

    CREATE

---
+++ index.js
@@ -0,0 +1,29 @@
+require('rootpath')();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const configuracion = require('config.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+morgan(':method :url :status :res[content-length] - :response-time ms');
+
+
+const controladorUsuario = require('./controller/ADMINISTRADOR/UsuarioController.js');
+const controladorProveedor = require('./controller/ADMINISTRADOR/ProveedorController.js'); 
+const controladorVenta = require('./controller/EMPLEADO/VentaController.js');
+
+app.use('/api/usuario', controladorUsuario);
+app.use('/api/proveedor', controladorProveedor); 
+app.use('/api/venta', controladorVenta );
+
+
+app.listen(configuracion.server.port, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Servidor escuchando en el puerto " + configuracion.server.port);
+    }
+});
+
